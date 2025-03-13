fb09e09234503604ef0c67a8b44a6bc238e3d129
Francisco Gindre <francisco.gindre@gmail.com>
2025-03-13 18:38:32 -0300

    Initial ZecDev Nextra Docs Site

---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+    logo: <span style={{ fontWeight: 800 }}>ZecDev</span>,
+    project: {
+      link: 'https://github.com/zecdev'
+    },
+    docsRepositoryBase: 'https://github.com/zecdev/zecdev.github.io'
+  }
