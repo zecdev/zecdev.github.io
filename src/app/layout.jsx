4dc09be386aa337685d82c34f5a845be9c1b795e
Francisco Gindre <francisco.gindre@gmail.com>
2025-03-14 09:18:14 -0300

    Add Favicon, basic structure

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -37,7 +37,7 @@ export default async function RootLayout({ children }) {
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          docsRepositoryBase="https://github.com/zecdev/zecdev.github.io/tree/main/src/"
           footer={footer}
           // ... Your additional layout options
         >
