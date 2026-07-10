f9a26e194239e63a1ecb0b62e954f89e4efb0b96
Pacu <pacu@zecdev.org>
2026-07-10 12:00:29 +0200

    Update Resources for Zcashd deprecation, Zebra, Zaino and Zallet

--- src/content/zcash-z3/_meta.js
+++ src/content/zcash-z3/_meta.js
@@ -1,7 +1,7 @@
 export default {
     index: 'What Is Z to the three',
     dashboard: '',
-    zcashd_cycle: 'Zcashd version lifecycle',
+    zcashd_cycle: 'Zcashd End-of-Life',
     contributing: '',
     resources: '',
     updates: 'Weekly Updates'
